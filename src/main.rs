ba7f25cc4673e1481b6cc25c521ef9218bb7fa38
Matevž Knez <64921080+Tevzi26@users.noreply.github.com>
2021-07-19 15:04:50 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Enter your guess!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Guess can only be a nuber!");
+                continue;
+            }
+        };
+    
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
